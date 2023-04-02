f98e07e922e3091fd6629a87acda6182b28d46f2
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-04-02 23:56:30 +0430

    The habitation structure of the landing component was added.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="containerC">
+        <img id='back' src={background} alt="background image"/>
+        <div className="container2">
+       <div className="topContainer">
+           <h2>Digital currency exchange</h2>
+       <input type="search" value={search} placeholder="search what you want !"/>
+       </div>
+       <div className="bottomContainer">
+        
+       </div>  </div>
+   </div>
     );
 };
 
