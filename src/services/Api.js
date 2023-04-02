bf65bce598206e71ae1ec893dfcae83884321a2f
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-04-02 17:12:27 +0430

    The API function was added in the services folder, which receives data from coingecko.

---
+++ src/services/Api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const BASE_URL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+export const api= async ()=>{
+    const response= await axios.get(BASE_URL);
+    return response;    
+}
